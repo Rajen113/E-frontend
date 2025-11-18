d7a7ef337244dbc26a652fba60cd6a0447422add
Monika-kushwah160 <149245533+Monika-kushwah160@users.noreply.github.com>
2025-11-18 18:21:34 +0530

    Features/admin page (#17)
    * admin-features
    
    * admin profile and edit prod,category
    
    * no chnages
    
    * connect with backend and set category and product
    
    * admin register,login

--- src/components/admin/header/Header.jsx
+++ src/components/admin/header/Header.jsx
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from '../../../context/AuthContext';
 
 function Header() {
-  const { isAdminLoggedIn, adminLogout } = useContext(AuthContext);
+  const [isLoggedIn, setIsLoggedIn] = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleAdminLogout = () => {
