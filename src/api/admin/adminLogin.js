2425e4750acbe0230ad7d3ecd8a44cf9861c0180
monika <kushwahmonika218@gmail.com>
2025-11-25 12:49:57 +0530

    admin login

--- src/api/admin/adminLogin.js
+++ src/api/admin/adminLogin.js
@@ -1,5 +1,9 @@
 import { authAPI } from "../instances";
 
 export const AdminLoginAPI = (formData) =>
-  authAPI.post("/admin/login", formData);
+  authAPI.post("/admin/login", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
